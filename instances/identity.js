460e2ab89f95e0935ca86d6945eeab4a9e77ae7c
Adam C. Baker <adam.durandal@gmail.com>
2014-05-06 22:01:10 -0700

    style changes
    lowercase objects that aren't constructors.
    clean trailing whitespace.
    consistent 2 space indentation.

--- instances/identity.js
+++ instances/identity.js
@@ -1,33 +1,33 @@
 var Constructor = require('../util').Constructor;
 
 var Id = Constructor(function(a) {
-	this.value = a;
+  this.value = a;
 });
 
 Id.prototype.concat = function(b) {
-	return new Id(concat(this.value, b.value));
+  return new Id(concat(this.value, b.value));
 };
 
 var runIdentity = function(i) { return i.value; };
 
 Id.prototype.empty = function() {
-	return new Id(this.value.empty());
+  return new Id(this.value.empty());
 };
 
 Id.prototype.map = function(f) {
-	return new Id(f(this.value));
+  return new Id(f(this.value));
 };
 
 Id.prototype.ap = function(b) {
-	return new Id(this.value(b.value));
+  return new Id(this.value(b.value));
 };
 
 Id.prototype.chain = function(f) {
-	return f(this.value);
+  return f(this.value);
 };
 
 Id.prototype.of = function(a) {
-	return new Id(a);
+  return new Id(a);
 };
 
 module.exports = {Identity: Id, runIdentity: runIdentity};
