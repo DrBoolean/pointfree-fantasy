622ca19214f275bfd619ed882f57b6e152d794ee
Brian <brian@looprecur.com>
2014-08-29 14:00:43 -0700

    Fix Const

--- instances/const.js
+++ instances/const.js
@@ -8,22 +8,22 @@ var Const = function(val) {
   return new ConstType(val);
 };
 
-Const.of = Const;
-ConstType.prototype.of = Const;
 
-Const.prototype.map = function(f) {
+ConstType.prototype.map = function(f) {
 	return Const(this.val);
 };
 
 // is const an applicative?
 // only if x is a monoid
-Const.prototype.of = function(x) {
+ConstType.prototype.of = function(x) {
 	return Const(x.empty());
 };
 
-Const.prototype.ap = function(c2) {
+ConstType.prototype.ap = function(c2) {
 	return Const(this.val.concat(c2.val));
 };
+
+Const.of = ConstType.prototype.of;
 
 // const is not a monad
 
